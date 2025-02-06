78168790f010e36112b22f89c5b511bdd98465b0
Julianq650 <ferchoquiroga5@gmail.com>
2025-02-06 15:18:25 -0500

    Add files via upload

---
+++ 1.2datos.py
@@ -0,0 +1,14 @@
+#usando python elabore un programa que reciba 2 numeros distintos e indique cual es el mayor 
+num1=float(input("ingrese el primer numero:"))
+num2=float(input("ingrese el segundo numero :"))
+def mayor(num1,num2):
+    
+ if num1>num2:
+    print(f" el MAYOR ES: {num1}") 
+ else:
+    print(f" el MAYOR ES : {num2}")
+    
+    
+    
+    #res=mayor(num1,num2)
+    #print({res})
---
+++ 1datos.py
@@ -0,0 +1,11 @@
+#atravez de los ejercicios planteados debera identificar resultados datos y procesos adicional a ellos debera explicar cada una de las instruccuines descritas dentro del codigo y su proposito en el  mismo 
+def perit(a,b,c):
+     return a+b+c
+
+lado1= float(input("ingrese la longitud del lado 1:"))
+lado2= float(input("ingrese la longitud del lado 2:"))
+lado3= float(input("ingrese la longitud del lado 3:"))
+
+perit =perit(lado1 , lado2 , lado3 )
+
+print(f" el resultado es: {perit}") 
