b07e29ebf5abc3d130589cff2c3dd871242291bb
Julianq650 <ferchoquiroga5@gmail.com>
2025-02-16 21:22:34 -0500

    Add files via upload

---
+++ 1.5variables.py
@@ -0,0 +1,23 @@
+x = 5
+y = "Hello, World!"
+#This is a comment.
+print (y)
+print (x)
+print (x,y)
+"""
+    2
+    1
+    3
+    """
+x = str(3)    
+y = int(3)    
+z = float(3)  
+
+
+
+b=13.3*y
+c=13/z
+print (int(b),c)
+print(type(x),"la variable z es de este tipo ")
+A=4
+a=4
